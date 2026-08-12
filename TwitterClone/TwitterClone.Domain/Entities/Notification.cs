5aa02ed3339207551811b218be0049bcb5ceb454
= <=mhsohag047@gmail.com>
2026-08-12 20:07:14 +0600

    Add Notification Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Notification.cs
@@ -0,0 +1,24 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Notification
+    {
+        private Guid _userId;
+        private string _message;
+        private DateTime _createdAt;
+
+        public Guid UserId
+        {
+            get { return _userId; }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+        }
+
+        public DateTime CreatedAt
+        {
+            get { return _createdAt; }
+        }
+    }
+}
