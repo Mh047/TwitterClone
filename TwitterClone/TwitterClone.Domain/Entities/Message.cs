ab305e60499d568f75ada49d29b512e8f5726045
= <=mhsohag047@gmail.com>
2026-08-12 20:08:34 +0600

    Add Message Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/Message.cs
@@ -0,0 +1,30 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class Message
+    {
+        private Guid _senderId;
+        private Guid _receiverId;
+        private string _content;
+        private DateTime _sentAt;
+
+        public Guid SenderId
+        {
+            get { return _senderId; }
+        }
+
+        public Guid ReceiverId
+        {
+            get { return _receiverId; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+        }
+
+        public DateTime SentAt
+        {
+            get { return _sentAt; }
+        }
+    }
+}
