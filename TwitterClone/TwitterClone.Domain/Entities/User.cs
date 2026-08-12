10aaf155a0f84019b3925a0757a2e8648e5cda86
= <=mhsohag047@gmail.com>
2026-08-12 19:38:29 +0600

    Add User Class

---
+++ TwitterClone/TwitterClone.Domain/Entities/User.cs
@@ -0,0 +1,27 @@
+﻿namespace TwitterClone.Domain.Entities
+{
+    public class User
+    {
+        private Guid _id;
+        private string _firstName;
+        private string _lastName;
+        private string _email;
+
+        public User()
+        {
+            _id = Guid.NewGuid();
+        }
+
+        public Guid Id
+        {
+            get {  return _id; }
+        }
+
+        public string Firstname
+        {
+            get { return _firstName; }
+            set { _firstName  = value; }
+        }
+
+    }
+}
