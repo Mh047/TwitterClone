722d6e9fb623fbf8af79245e4ddc341fd099142f
Mir Hossain Sohag <mhsohag047@gmail.com>
2026-08-15 19:46:19 +0600

    Update Like.cs

--- TwitterClone/TwitterClone.Domain/Entities/Like.cs
+++ TwitterClone/TwitterClone.Domain/Entities/Like.cs
@@ -3,9 +3,16 @@ namespace TwitterClone.Domain.Entities
 {
     public class Like
     {
+        public Guid _id;
         private Guid _userId;
         private Guid _tweetId;
         private DateTime _likedAt;
+
+        public Guid Id
+        {
+            get { return _id; }
+        }
+        
         public Guid UserId
         {
             get { return _userId; }
